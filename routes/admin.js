9c4f75754ee7b63fa5c5c25a5e04a4a44ab9b6b1
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-15 15:35:16 -0300

    Update dashboard with datas in database

--- routes/admin.js
+++ routes/admin.js
@@ -31,8 +31,13 @@ router.use(function(req, res, next){
 
 router.get('/', function(req, res, next) {
 
-  res.render('admin/index', admin.getParams(req));
-
+  admin.dashboard().then(data => {
+    res.render('admin/index', admin.getParams(req, {
+      data 
+    }));
+  }).catch(err =>{
+    console.error(err);
+  });
 });
 
 router.get('/login', function(req, res, next) {
