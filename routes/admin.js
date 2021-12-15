be426a94aeea2ea0465fb4c59d77715d71272ffb
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2021-12-15 15:01:54 -0300

    Routes menu - links/href

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,9 @@
 const conn = require('./../inc/db');
-var users = require('./../inc/users');
 var express = require('express');
+
+var users = require('./../inc/users');
+var admin = require('./../inc/admin');
+
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -12,9 +15,25 @@ router.use(function(req, res, next){
   }
 });
 
+router.get('/logout', function(req, res, next){
+
+  delete req.session.user;
+  res.redirect('/admin/login');
+});
+
+router.use(function(req, res, next){
+
+  req.menus = admin.getMenus();
+
+  next();
+
+});
+
 router.get('/', function(req, res, next) {
 
-  res.render('admin/index');
+  res.render('admin/index', {
+    menus: req.menus
+  });
 
 });
 
@@ -43,37 +62,40 @@ router.post('/login', function(req, res, next){
   }
 });
 
-router.get('/logout', function(req, res, next){
-
-  delete req.session.user;
-  res.redirect('/admin/login');
-});
-
 router.get('/contacts', function(req, res, next) {
 
-  res.render('admin/contacts');
+  res.render('admin/contacts', {
+    menus: req.menus
+  });
 });
 
 router.get('/emails', function(req, res, next) {
 
-  res.render('admin/emails');
+  res.render('admin/emails', {
+    menus: req.menus
+  });
 });
 
 router.get('/menus', function(req, res, next) {
 
-  res.render('admin/menus');
+  res.render('admin/menus', {
+    menus: req.menus
+  });
 });
 
 router.get('/reservations', function(req, res, next) {
 
   res.render('admin/reservations', {
-    date: {}
+    date: {},
+    menus: req.menus
   });
 });
 
 router.get('/users', function(req, res, next) {
 
-  res.render('admin/users');
+  res.render('admin/users', {
+    menus: req.menus
+  });
 });
 
 module.exports = router;
